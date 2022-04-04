43c2d957aab5048c216a89e9241eea25c3baf1a1
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-04 14:33:20 +0600

    chart added done

--- src/Components/LineChart/WebsiteLineChart.jsx
+++ src/Components/LineChart/WebsiteLineChart.jsx
@@ -10,6 +10,10 @@ import {
   ResponsiveContainer,
   AreaChart,
   Area,
+  BarChart,
+  Bar,
+  PieChart,
+  Pie,
 } from "recharts";
 
 const WebsiteLineChart = () => {
@@ -52,9 +56,12 @@ const WebsiteLineChart = () => {
     },
   ];
   return (
-    <div className="row">
+    <div className="row mt-5">
       <div className="col-md-6">
-        <LineChart width={400} height={300} data={data}>
+        <h5 className="text-center fw-bold text-primary mb-3">
+          MONTH WISE SELL
+        </h5>
+        <LineChart width={300} height={200} data={data}>
           <XAxis dataKey="month"></XAxis>
           <Line
             type="monotone"
@@ -69,26 +76,69 @@ const WebsiteLineChart = () => {
       </div>
 
       <div className="col-md-6">
-        <AreaChart width={400} height={300} data={data}>
+        <h5 className="text-center fw-bold text-primary mb-3">
+          INVESTMENT VS REVENUE
+        </h5>
+        <AreaChart width={300} height={200} data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="month"></XAxis>
           <Area
             type="monotone"
             dataKey="investment"
             stroke="#8884d8"
             fill="#8884d8"
-            fillOpacity={0.3}
+            stackId="1"
           />
           <Area
             type="monotone"
             dataKey="revenue"
             stroke="#137ae0"
             fill="#137ae0"
-            fillOpacity={0.3}
+            stackId="1"
           />
           <YAxis></YAxis>
           <Tooltip></Tooltip>
-          <Legend></Legend>
         </AreaChart>
+      </div>
+      <div className="col-md-6 mt-5">
+        <h5 className="text-center fw-bold text-primary mb-3">
+          INVESTMENT VS REVENUE
+        </h5>
+        <BarChart width={300} height={200} data={data}>
+          <XAxis dataKey="month"></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+
+          <Bar dataKey="investment" stackId="a" fill="#8a87f3" />
+          <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+        </BarChart>
+      </div>
+      <div className="col-md-6 mt-5">
+        <h5 className="text-center fw-bold text-primary mb-3">
+          INVESTMENT VS REVENUE
+        </h5>
+        <PieChart width={300} height={300}>
+          <Tooltip></Tooltip>
+
+          <Pie
+            data={data}
+            dataKey="investment"
+            cx="50%"
+            cy="50%"
+            outerRadius={60}
+            fill="#8884d8"
+          />
+          <Pie
+            data={data}
+            dataKey="revenue"
+            cx="50%"
+            cy="50%"
+            innerRadius={70}
+            outerRadius={90}
+            fill="#82ca9d"
+            label
+          />
+        </PieChart>
       </div>
     </div>
   );
