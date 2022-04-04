8338d728a4ae6fbaeb50022be39da422546238a0
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-04 10:17:45 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Blogs from "./Components/Blogs/Blogs";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="*" element={<About />}></Route>
+      </Routes>
     </div>
   );
 }
